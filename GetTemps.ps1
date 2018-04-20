e2329653dc050cd94489d500ca91afa7e69fb30f
babgvant <avantil@gmail.com>
2018-04-20 09:58:25 -0500

    also log switch state

--- GetTemps.ps1
+++ GetTemps.ps1
@@ -1,2 +1,2 @@
 $request = 'http://192.168.2.227/get'
-Invoke-WebRequest $request | ConvertFrom-Json | %{ "{0}, {1}" -f $_.temp, $_.humidity } | Out-File templog.txt -Append
+Invoke-WebRequest $request | ConvertFrom-Json | %{ "{0}, {1}, {2}" -f $_.temp, $_.humidity, $_.switch_state } | Out-File templog.txt -Append
